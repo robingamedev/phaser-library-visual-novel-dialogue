37b36c5b5a30fb5a069817077f19181bc2001d2e
your.user.name <153454908+robingamedev@users.noreply.github.com>
2025-07-15 20:10:18 -0700

    fix: update to include it all.

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export { default } from './VisualNovelDialogue';
+export type {
+  DialogueConfig,
+  TextStyle,
+  ParsedStyle,
+  DialogueSettings,
+  Character,
+  DialogueScript,
+  ChoiceCommand,
+  DialogueData
+} from './VisualNovelDialogue'; 
